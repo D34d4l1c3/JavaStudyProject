0e24b4cbc7b6d596bae362265de2b0a3cf71564a
D34d4l1c3 <Ladyelune@gmail.com>
2023-07-26 19:48:10 +0300

    Практика задача 1 первого блока - решение Task.java

--- src/BasicJava/Chapter1/Task.java
+++ src/BasicJava/Chapter1/Task.java
@@ -1,6 +1,21 @@
 package BasicJava.Chapter1;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 //TODO:Практика:
 // В методе main консольного приложения извлечь числа из текста (текст передать параметром при запуске приложения).
 // Результат программы выводит сумму найденных чисел в консоль.
 public class Task {
+    public static void main(String args[]){
+        System.out.println("Оригинальная строка: "+args[0]);
+        Double sum = 0d;
+//        String t[] = {"234sdsads 3 sdsads -54 dsfdsf 44.3 sdsd 55/3 343,7","rrrrrrrrrr"}; //Тестовая строка
+        Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?(\\,\\d+)?");
+        Matcher matcher = pattern.matcher(args[0]);
+        while (matcher.find()){
+            sum = sum + Double.valueOf(matcher.group().replaceAll(",","."));
+        }
+        System.out.println(sum);
+    }
 }
